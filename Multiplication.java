3383301fcb3cd92cd518a2c4d640f3c1ec3843e9
Avesh Shaikh <97118756+AviiiSk@users.noreply.github.com>
2023-01-16 02:08:22 +0530

    Add files via upload
    Multiplication

---
+++ Multiplication.java
@@ -0,0 +1,31 @@
+
+// 3. Write the java program to design method for multiplication of two number ,
+// which returns int value to that method and result should be print into main method.
+
+
+
+
+
+
+public class Multiplication {
+
+	@SuppressWarnings("unused")
+	public static void subtract(int a, int b)
+	{
+		int c  = a*b;
+		
+		System.out.println("Multiplication of Two Numbers : " + c);
+		
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+		subtract(30, 15);
+		
+		
+
+	}
+
+}
